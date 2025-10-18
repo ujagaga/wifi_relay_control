9c0eb51779c40ccc738706cdd8b4af75a5d2362c
Rada Berar <ujagaga@gmail.com>
2025-10-18 13:15:32 +0200

    add gunicorn

--- relay_server/install.sh
+++ relay_server/install.sh
@@ -30,7 +30,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Installing Python packages..."
-pip3 install flask authlib flask-wtf requests
+pip3 install flask authlib flask-wtf requests gunicorn
 if [ $? -ne 0 ]; then
   echo "Error: Failed to install python libraries. Aborting installation."
   exit 1
